3173c98efa6b691198678dcb861ba6ad37f1bc60
Roberto Gonzalez <robergonzalez@ebay.com>
2019-11-30 16:15:34 -0800

    Log In View

--- socialMediaAlert/ContentView.swift
+++ socialMediaAlert/ContentView.swift
@@ -9,9 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var onboardShowing = true
+
     var body: some View {
-        Text("Hello, World!")
-    }
+        NavigationView {
+                   VStack(alignment: .leading, spacing: 0) {
+
+                       Spacer()
+                           .frame(minHeight: 24, idealHeight: 80, maxHeight: 80)
+
+                       Group {
+                           Text("SocialMedia Alert")
+//                                .foregroundColor(Color.red)
+                       }
+//                       .modifier(MarketSans(.largeTitle))
+                       .layoutPriority(1.0)
+
+                       Spacer()
+                           .frame(minHeight: 12, idealHeight: 48, maxHeight: 48)
+
+                           HStack(spacing: 0) {
+                               Image("self.data.item[num].image")
+                                   .resizable()
+                                   .frame(minWidth: 40, idealWidth: 56, maxWidth: 56, minHeight: 40, idealHeight: 56, maxHeight: 56)
+                                   .aspectRatio(1, contentMode: .fit)
+                                   .padding(.trailing, 24)
+                               VStack(alignment: .leading, spacing: 0) {
+                                   Text("self.data.item[num].title")
+                                       .font(.headline)
+                                       .padding(.bottom, 4)
+                                   Text("self.data.item[num].description")
+                                       .font(.subheadline)
+//                                       .foregroundColor(.textGrey)
+                               }
+                           }
+                           Spacer()
+                               .frame(minHeight: 12, idealHeight: 40, maxHeight: 40)
+                       
+                       Spacer()
+                           .frame(minHeight: 20, maxHeight: .infinity)
+
+                       Button(action: {self.onboardShowing = false}, label: {Text("Continue")})
+//                           .buttonStyle(PrimaryButton())
+                           .padding(.bottom, 16)
+//                    .modifier(MarketSans(.headline))
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color.accentColor)
+                   }
+                   .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                   .padding(.horizontal, 16)
+                   .accentColor(.red)
+               }.opacity(onboardShowing ? 100 : 0)    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ socialMediaAlert/LogInView.swift
@@ -0,0 +1,82 @@
+//
+//  LogInView.swift
+//  socialMediaAlert
+//
+//  Created by Roberto Gonzalez on 11/30/19.
+//  Copyright © 2019 Roberto Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct LogInView: View {
+    // MARK: - Propertiers
+    @State private var email = ""
+    @State private var password = ""
+    
+    // MARK: - View
+    var body: some View {
+        VStack() {
+            Text("Social Alert")
+                .font(.largeTitle).foregroundColor(Color.white)
+                .padding([.top, .bottom], 40)
+                .shadow(radius: 10.0, x: 20, y: 10)
+            
+            Image("logoAlert")
+                .resizable()
+            .aspectRatio(contentMode: .fill)
+                .frame(width: 250, height: 250)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10.0, x: 20, y: 10)
+                .padding(.bottom, 50)
+            
+            VStack(alignment: .leading, spacing: 15) {
+                TextField("Email", text: self.$email)
+//                    .padding()
+//                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+                
+                SecureField("Password", text: self.$password)
+//                    .padding()
+//                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding([.leading, .trailing], 27.5)
+            
+            Button(action: {}) {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding(.top, 50)
+            
+            Spacer()
+            HStack(spacing: 0) {
+                Text("Don't have an account? ")
+                    .colorInvert()
+                Button(action: {}) {
+                    Text("Sign Up")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25))
+
+                }
+            }
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all))
+        
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
